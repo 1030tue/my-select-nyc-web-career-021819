65bb39fcec4384a53055a393a912c7f9d626ca5e
Kate <katekim5809@gmail.com>
2019-02-06 17:30:53 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   ary=[]
   i = 0
   while i < collection.length
-  if yield(collection[i]) == true
+  if yield(collection[i]) = true
     ary << collection[i]
     i+=1
   end
